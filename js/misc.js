a3fe764dc67aa1679bedfc60ae3e5f787960e186
anton_agafonov <toonagafonov@gmail.com>
2023-10-10 14:11:52 +0300

    c

--- js/misc.js
+++ js/misc.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // Set camera position
-camera.position.set(0, 0, 5);
+camera.position.set(0.5,0.2,0.5);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
